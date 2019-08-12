f4f5926d064b52af8fdcdc31523d72cc045e6e27
hirokazutei <hirokazu.tei@gmail.com>
2019-08-12 11:28:51 +0900

    FEAT: Changed the default colors since they were too bright

--- src/color.ts
+++ src/color.ts
@@ -2,11 +2,11 @@ type DefaultDebugColorKeys = "stack" | "queue" | "inset";
 type DefaultDebugBorderKeys = "stack" | "queue";
 
 export const defaultDebugColor: { [key in DefaultDebugColorKeys]: string } = {
-  stack: "#00FFFF",
-  queue: "#FF00FF",
-  inset: "#FFFF00"
+  stack: "#45e6e6",
+  queue: "#e645e6",
+  inset: "#e6e645"
 };
 export const defaultBorderColor: { [key in DefaultDebugBorderKeys]: string } = {
-  stack: "#0080ff",
-  queue: "#ff0080"
+  stack: "#45b8b8",
+  queue: "#b845b8"
 };
