3aaf858ee5be1e1f9a298d1dd3a96e3997b6dd9d
hirokazutei <hirokazu.tei@gmail.com>
2019-08-23 18:25:48 +0900

    FEAT: Added a react-native example with flow

---
+++ examples/FlowProjectExample/src/components/atoms/Stack.js
@@ -0,0 +1,7 @@
+/* @flow */
+import {stackFactory} from 'react-native-spacing-system';
+import {spacing, type Spacing} from '../../constants/spacing';
+
+const stack = stackFactory<Spacing>(spacing);
+
+export default stack;
