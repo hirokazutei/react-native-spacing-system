1cdd4e7503a831ff9467c2eb1fdcbe7d34fb50c9
Hirokazu Tei <hirokazutei@nextdoor.com>
2020-05-12 10:03:42 -0400

    CHORE: Generated lib files

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -9,5 +9,5 @@ import { StackDebugOptions, StackProps, StackStyles } from "./Stack/stackTypes";
 import Queue from "./Queue";
 import queueFactory from "./Queue/queueFactory";
 import { QueueDebugOptions, QueueProps, QueueStyles } from "./Queue/queueTypes";
-export { DebugContext, DebugContextProps, InsetDebugItemProps, DebugItemProps, Inset, insetFactory, All, InsetDebugOptions, Horizontal, InsetProps, Other, PaddingPossibilities, VerHor, Vertical, InsetStyles, Stack, stackFactory, StackDebugOptions, StackProps, StackStyles, Queue, queueFactory, QueueDebugOptions, QueueProps, QueueStyles };
+export { DebugContext, DebugContextProps, InsetDebugItemProps, DebugItemProps, Inset, insetFactory, All, InsetDebugOptions, Horizontal, InsetProps, Other, PaddingPossibilities, VerHor, Vertical, InsetStyles, Stack, stackFactory, StackDebugOptions, StackProps, StackStyles, Queue, queueFactory, QueueDebugOptions, QueueProps, QueueStyles, };
 //# sourceMappingURL=index.d.ts.map
