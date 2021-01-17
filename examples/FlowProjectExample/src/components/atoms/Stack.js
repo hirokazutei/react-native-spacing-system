7464c40bc7c5c6c1cf9c24c25a5930d4c1ed5d8f
Hirokazu Tei <hirokazutei@nextdoor.com>
2021-01-16 19:16:48 -0500

    CHORE: move-flow-example-to-different-repository

--- examples/FlowProjectExample/src/components/atoms/Stack.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-import {stackFactory} from 'react-native-spacing-system';
-import {spacing, type Spacing} from '../../constants/spacing';
-
-const stack = stackFactory<Spacing>(spacing);
-
-export default stack;
