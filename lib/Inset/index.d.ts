cabf846e4e2c0a2570d88cdaae1213003bdd6d6f
Hirokazu Tei <hirokazutei@nextdoor.com>
2021-01-22 02:46:02 -0500

    CHORE: generate lib code

--- lib/Inset/index.d.ts
+++ lib/Inset/index.d.ts
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { View } from "react-native";
 import { InsetProps } from "./insetTypes";
-declare const Inset: (props: InsetProps<number, false>) => React.CElement<import("react-native").ViewProps, View>;
+declare const Inset: ({ layout, children, onLayout, _debug, _debugOptions, ...paddings }: InsetProps<number, false>) => React.CElement<import("react-native").ViewProps, View>;
 export default Inset;
 //# sourceMappingURL=index.d.ts.map
